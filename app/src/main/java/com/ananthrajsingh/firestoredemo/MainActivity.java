0a2834839da44288106281e109a00850229f1248
Ananth Raj Singh <ananthraj.singh@gmail.com>
2018-05-18 18:51:04 +0530

    TextView linked to Firebase Realtime Database.

--- app/src/main/java/com/ananthrajsingh/firestoredemo/MainActivity.java
+++ app/src/main/java/com/ananthrajsingh/firestoredemo/MainActivity.java
@@ -1,13 +1,42 @@
 package com.ananthrajsingh.firestoredemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView nameTextView;
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mNameRef = mRootRef.child("name");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameTextView = (TextView) findViewById(R.id.textViewName);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mNameRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                nameTextView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
