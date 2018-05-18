9179a804f4774c174708ce99b391dac54019965c
Ananth Raj Singh <ananthraj.singh@gmail.com>
2018-05-18 13:25:15 +0530

    Initial commit

---
+++ app/src/main/java/com/ananthrajsingh/firestoredemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ananthrajsingh.firestoredemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
